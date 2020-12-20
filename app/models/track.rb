b68199051c9da2f1aa15b6fd98eb697870c8269b
ilia zolas <ilia@thedmcsa.co.za>
2020-12-20 16:54:41 +0100

    test 4

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,6 @@
 class Track < ApplicationRecord
-   
-   
-    belongs_to :playlist, through: :playlists_tracks
+    belongs_to :album
+    belongs_to :user
+    
     has_many :tags, through: :tags_tracks
 end
