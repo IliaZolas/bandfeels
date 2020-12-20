9eeaf6fc6176277e400f03623fd6808b3aa1ceb0
ilia zolas <ilia@thedmcsa.co.za>
2020-12-20 16:48:58 +0100

    heroku test 2

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,6 @@
 class Track < ApplicationRecord
-    belongs_to :album
    
+    belongs_to :user
     belongs_to :playlist, through: :playlists_tracks
     has_many :tags, through: :tags_tracks
 end
