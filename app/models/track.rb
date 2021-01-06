b06464f1c90fe1d6e0beeba8ccd97772d65c40a5
ilia zolas <ilia@thedmcsa.co.za>
2021-01-06 19:12:12 +0100

    has one attached audio

--- app/models/track.rb
+++ app/models/track.rb
@@ -7,4 +7,6 @@ class Track < ApplicationRecord
     validates :title, presence: true, length: { in: 1..20 }
     validates :description, presence: true, length: { in: 10..60 }
     has_one_attached :photo
+    has_attached_file :audio
+#   validates_attachment_content_type :audio, content_type: [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
 end
