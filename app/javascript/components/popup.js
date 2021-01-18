9a5d2a444d5be04f2608563ae0bbf509d408fe45
ilia zolas <ilia@thedmcsa.co.za>
2021-01-18 15:07:23 +0100

    popup append with JS

--- app/javascript/components/popup.js
+++ app/javascript/components/popup.js
@@ -4,13 +4,15 @@ const pressAddToPlaylist = () => {
 
     cards.forEach((card) => {
         card.querySelector(".addtoplaylist").addEventListener("click", function () {
-            // card.querySelector("#popup");
-            // card.querySelector(".play-button").className += " hidden";
-            card.querySelector(".pop-up").className =
-                card.querySelector(".pop-up").className.replace
-                (/(?:^|\s)hidden-popup(?!\S)/g, '');
+            card.querySelector(".pop-up").className = card.querySelector(".pop-up").className.replace(/(?:^|\s)hidden-popup(?!\S)/g, '');
         });
     });
 }
 
 export { pressAddToPlaylist };
+
+
+// document.querySelectorAll('trackpopup').appendChild(document.getElementById('popup')
+
+
+// card.querySelcetor(".pop-up").destination.appendChild(trackpopup);
