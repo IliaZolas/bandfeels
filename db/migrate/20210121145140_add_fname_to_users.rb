f5d96c8c4edac0ce6ef9e59b368c4acdb35c1a51
ilia zolas <ilia@thedmcsa.co.za>
2021-01-21 16:11:22 +0100

    updatng devise and user schema

---
+++ db/migrate/20210121145140_add_fname_to_users.rb
@@ -0,0 +1,7 @@
+class AddFnameToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :fname, :string
+    add_column :users, :lname, :string
+    add_column :users, :username, :string
+  end
+end
