65b457d6c019a56a2d18455cc6f125ea042596be
ilia zolas <ilia@thedmcsa.co.za>
2021-01-21 16:34:49 +0100

    fixed signup and edit for devise and account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname, :username])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fname, :lname, :username])
+  end
 end
