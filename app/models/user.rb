123c33ce90d72960e763853205397b176ce1fbfe
ilia zolas <ilia@thedmcsa.co.za>
2021-01-29 17:56:13 +0100

    upload avatar works

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
-          
+
   has_many :playlists
   has_many :tracks
   has_many :albums
+  has_one_attached :photo
 end
