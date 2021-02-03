608b9bb5cb509d7d6020936e442d14e92686a442
ilia zolas <ilia@thedmcsa.co.za>
2021-02-03 11:34:23 +0100

    refactored js open

--- app/javascript/components/popup.js
+++ app/javascript/components/popup.js
@@ -3,34 +3,10 @@ const pressAddToPlaylist = () => {
 
     cards.forEach((card) => { 
         card.querySelector(".addtoplaylist").addEventListener("click", function () {
-            card.querySelector(".pop-up").className = card.querySelector(".pop-up").className.replace(/(?:^|\s)hidden-popup(?!\S)/g, '');
-            document.querySelector('#trackpopup').appendChild(document.querySelector('.pop-up'));
+            card.querySelector("#popup").classList.replace('hidden-popup', 'none');
+            document.querySelector('#trackpopup').appendChild(document.querySelector('#popup'));
         });
     });
 }
 
 export { pressAddToPlaylist };
-
-
-// document.querySelectorAll('trackpopup').appendChild(document.getElementById('popup')
-
-
-// card.querySelcetor(".pop-up").destination.appendChild(trackpopup);
-
-
-// simon
-
-// cards.forEach((card) => {
-//     card.querySelector(".addtoplaylist").addEventListener("click", (event) => {
-//         // card.querySelector(".pop-up").className = card.querySelector(".pop-up").className.replace(/(?:^|\s)hidden-popup(?!\S)/g, '');
-//         document.querySelector(".pop-up");
-//         const playlistForm = document.querySelectorAll(".playlist-form");
-//         playlistForm.forEach(playlist => {
-//             document.querySelector(".pop-up").className.remove("hidden-popup");
-//         })
-
-
-//         // .className.remove("hidden-popup");
-//     });
-// });
-// }
