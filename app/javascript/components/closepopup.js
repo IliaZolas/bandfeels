d2b7124b154572d4a3d99a83172b179f4edbc446
ilia zolas <ilia@thedmcsa.co.za>
2021-02-03 11:37:05 +0100

    js remove none

--- app/javascript/components/closepopup.js
+++ app/javascript/components/closepopup.js
@@ -4,8 +4,12 @@ const pressClosePopup = () => {
     popups.forEach((popup) => {
         popup.querySelector(".close-popup").addEventListener("click", function () {
             popup.classList.add("hidden-popup");
+            popup.classList.remove("none");
         });
     });
 }
 
 export { pressClosePopup };
+
+
+// document.querySelector('#trackpopup').appendChild(document.querySelector('.pop-up'));
