d6c884454b2013cdfa67eee4767dbfe72042c3bd
ilia zolas <ilia@thedmcsa.co.za>
2021-02-22 17:54:52 +0100

    cleaning for heroku

--- db/migrate/20210222130236_add_roles_to_users.rb
+++ db/migrate/20210222130236_add_roles_to_users.rb
@@ -1,5 +1,5 @@
 class AddRolesToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :role, :integer
+    # add_column :users, :role, :integer
   end
 end
--- db/migrate/20210222134822_remove_roles_from_users.rb
+++ db/migrate/20210222134822_remove_roles_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveRolesFromUsers < ActiveRecord::Migration[6.0]
   def change
-    remove_column :users, :roles, :string
+    # remove_column :users, :roles, :string
   end
 end
