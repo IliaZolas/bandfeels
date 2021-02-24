108c333b67fe0cb5b3af04ea3126546af2caf3e3
ilia zolas <ilia@thedmcsa.co.za>
2021-02-24 18:34:32 +0100

    small update

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,6 @@ Rails.application.routes.draw do
       resources :tracks 
     end
     
-    
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
